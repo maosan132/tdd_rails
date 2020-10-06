c36fd3d8a4f653ebf7f9c3d3a7302de947a02702
maosan132 <maosan132@gmail.com>
2020-10-05 21:27:40 -0500

    added tests for index routes

--- spec/features/listing_players_spec.rb
+++ spec/features/listing_players_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
-Rspec.describe 'list players' do
+RSpec.describe 'list players' do
   it 'displays the first and last names of all players saved' do
     player1 = Player.create(first_name: 'Eduardo', last_name: 'Baik')
     player2 = Player.create(first_name: 'Cris', last_name: 'Burns')
 
-    visits '/'
+    #visit '/'
+    visit root_path
+
+    expect(page).to have_text('Eduardo')
+    expect(page).to have_text('Baik')
+    expect(page).to have_text('Cris')
+    expect(page).to have_text('Burns')
+
+    expect(page).to have_link('New Player')
   end
 end
