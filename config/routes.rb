d7ec06d45b631eb34f739a6cb0b1846492231ad6
maosan132 <maosan132@gmail.com>
2020-10-08 15:02:44 -0500

    created users controller and model, created users index file and route to root, modified routes replacing all seven restful routes with resource keyword

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # get '/' => 'players#index'
-  root 'players#index'
-  get 'players' => 'players#index'
-  get 'players/new' => 'players#new'
-  post 'players' => 'players#create'
-  get 'players/:id' => 'players#show', as: 'player'
-  get 'players/:id/edit' => 'players#edit', as: 'player_edit'
-  patch 'players/:id' => 'players#update'
-  delete 'players/:id' => 'players#destroy'
+  root 'users#index'
+
+  resources :players
+  
+  #get 'players' => 'players#index'
+  #get 'players/new' => 'players#new'
+  #post 'players' => 'players#create'
+  #get 'players/:id' => 'players#show', as: 'player'
+  #get 'players/:id/edit' => 'players#edit', as: 'player_edit'
+  #patch 'players/:id' => 'players#update'
+  #delete 'players/:id' => 'players#destroy'
 end
